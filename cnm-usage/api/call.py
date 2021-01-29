4175642ee0c1ec1dfdb92e72025e2d01e1aee004
salkoritz <61440008+salkoritz@users.noreply.github.com>
2021-01-29 12:40:36 -0600

    added 'headers' attribute to Call class, added getPerformance method, added import requests

--- cnm-usage/api/call.py
+++ cnm-usage/api/call.py
@@ -1,4 +1,5 @@
 import auth
+import requests
 
 class Call:
     def __init__(self, host, client_id, client_secret, params):
@@ -6,9 +7,19 @@ class Call:
         self.token = auth.generate_api_session(host, client_id, client_secret)
         self.params = params
 
+        self.headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+        }
+
         def refreshToken(self):
             self.token = auth.generate_api_session(self.host, self.client_id, self.client_secret)
 
-#def getPerformance
+        def getPerformance(self):
+            api_url = 'https://{}/api/v1/devices/performance'.format(self.host)
+
+            r = requests.get(api_url, self.headers, self.params, verify=False)
+            auth.check_http_return("API", api_url, r.status_code, r)
+            return r.json()
+
 
 
