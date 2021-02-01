69b78ccb490f815a1dd5051daf8dddb0d98f7d69
salkoritz <61440008+salkoritz@users.noreply.github.com>
2021-02-01 15:52:12 -0600

    reorganized imports, merged auth into api package

--- cnm_usage/api/auth.py
+++ cnm_usage/api/auth.py
@@ -12,7 +12,6 @@ import sys
 import requests
 import json
 import base64
-from datetime import datetime, timedelta
 
 # LIVE: 208.93.184.19
 # TEST: 208.93.184.17
@@ -24,8 +23,10 @@ def check_http_return(section, url, code, request):
         print('URL: {}'.format(url))
         try:
             print(json.dumps(request.json(), indent=2))
-        except: pass
+        except:
+            pass
         sys.exit(1)
+
 
 # Retrieve access parameters (url, access_token, and expires_in).
 def get_access_parameters(host, client_id, client_secret):
@@ -33,13 +34,14 @@ def get_access_parameters(host, client_id, client_secret):
     encoded_credentials = base64.b64encode('{}:{}'.format(client_id, client_secret).encode()).decode()
     headers = {
         'Authorization': 'Basic {}'.format(encoded_credentials),
-        'Content-Type':'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     print(encoded_credentials)
     body = 'grant_type=client_credentials'
     r = requests.post(token_url, body, headers=headers, verify=False)
     check_http_return('Access Parameters', token_url, r.status_code, r)
     return r.json()['access_token'], r.json()['expires_in']
+
 
 # Validate the expiration of the access token.
 def validate_access_token(host, access_token):
@@ -51,18 +53,19 @@ def validate_access_token(host, access_token):
     check_http_return('Validate Access Token', validate_url, r.status_code, r)
     return r.json()['expires_in']
 
+
 def generate_api_session(host, client_id, client_secret):
     # Retrieve access parameters and generate API session
     print('\nRetrieve Access Parameters')
     access_token, expires_in = get_access_parameters(host, client_id, client_secret)
     print('Success: access_token ({}) expires_in ({})\n'.format(access_token, expires_in))
 
-
     # Validate time remaining for the access token
     print('Validating expiration time')
     expires_in_check = validate_access_token(host, access_token)
     print('Success: expiresIn ({})\n'.format(expires_in_check))
     return access_token
+
 
 # Execute API using URL returned in access parameters.
 def call_api(host, path, access_token):
--- cnm_usage/api/call.py
+++ cnm_usage/api/call.py
@@ -1,10 +1,11 @@
-import auth
 import requests
+from .auth import check_http_return, generate_api_session
+
 
 class Call:
     def __init__(self, host, client_id, client_secret, params):
         self.host = host
-        self.token = auth.generate_api_session(host, client_id, client_secret)
+        self.token = generate_api_session(host, client_id, client_secret)
         self.params = params
 
         self.headers = {
@@ -12,14 +13,11 @@ class Call:
         }
 
         def refreshToken(self):
-            self.token = auth.generate_api_session(self.host, self.client_id, self.client_secret)
+            self.token = generate_api_session(self.host, self.client_id, self.client_secret)
 
         def getPerformance(self):
             api_url = 'https://{}/api/v1/devices/performance'.format(self.host)
 
             r = requests.get(api_url, self.headers, self.params, verify=False)
-            auth.check_http_return("API", api_url, r.status_code, r)
+            check_http_return("API", api_url, r.status_code, r)
             return r.json()
-
-
-
