9900c85a6bb723dff97d6e304b37ad3ec921326f
salkoritz <61440008+salkoritz@users.noreply.github.com>
2021-02-19 16:22:44 -0600

    changed all quotes to single

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cnm_usage",
+    name='cnm_usage',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
@@ -14,6 +14,6 @@ setup(
         get_usage=cnm_usage.cli:main
     ''',
 
-    author="Sal Koritz",
-    description='cli app to pull usage data from cnMaestro API',
+    author='Sal Koritz',
+    description='cli app to pull usage data from cnMaestro API'
 )
